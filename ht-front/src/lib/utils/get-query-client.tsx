cd574ea22245f6422de28bf6dc11291b79b764ad
yyj0917 <yyj0917@yonsei.ac.kr>
2025-08-04 00:58:10 +0900

    init : project init settings 폴더 구조 & Github 워크플로우 등

---
+++ ht-front/src/lib/utils/get-query-client.tsx
@@ -0,0 +1,31 @@
+import { isServer, QueryClient } from '@tanstack/react-query';
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (isServer) {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
+
+// 서버 사이드에서는 무상태
+// 브라우저 환경에서는 유상태로 동작하게 함.
